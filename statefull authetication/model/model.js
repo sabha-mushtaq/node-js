79073445057ca45a57af3155cdd880fa04df14e3
sabha mushtaq <138353007+sabha-mushtaq@users.noreply.github.com>
2024-10-09 20:41:01 +0530

    Add files via upload

---
+++ statefull authetication/model/model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+name:{
+type:String,
+required: true, 
+
+
+},
+email:{
+
+type:String,
+required:true,
+unique: true,
+
+
+
+
+},
+password:{
+type:Number,
+required: true,
+
+
+
+},
+uuid:{
+
+    type:String,
+    required:true,
+    unique: true,
+
+
+},
+
+
+
+
+
+
+
+
+},{timestamps :true})
+
+const user = mongoose.model('user',userSchema);
+module.exports= user;
